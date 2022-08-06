82f5ae76be863639a13e3e5bfb898fc8242da2ab
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-06 15:23:00 -0300

    Update client.php

--- client.php
+++ client.php
@@ -5,32 +5,34 @@
     define("MSG_SIZE_LARGE", 8192);
     define("SOCK_PATH", "/home/ubuntu/GitHub/CppTrader/bin/daemons/test.sock");
 
-    // Create a Connection to Given Socket
-    function create_conn(string $path) {
+    /* Create a Connection to Socket */
+    function unix_socket(string $path) {
         $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
         if (!$socket) return false;
         if (!socket_connect($socket, $path)) return false;
         return $socket;
     }
 
+    /* Send Command to Stream */ 
     function send_command(Socket $socket, string $command) {
         return socket_write($socket, $command."\n", MSG_SIZE);
     }
 
+    /* Read Server Response */
     function read_response(Socket $socket, string $command) {
         return socket_read($socket, MSG_SIZE_LARGE);
     }
 
-    // Do commands
-    $conn = create_conn(SOCK_PATH);
-    if ($conn) {
-        send_command($conn, "add symbol 1 BTC_USDT");
-        send_command($conn, "add book 1");
-        send_command($conn, "add limit sell 1 1 20000 1");
-        echo(read_response($conn, "get book 1"));
-        send_command($conn, "add limit buy 1 1 20000 1");
-        echo(read_response($conn, "get book 1"));
-        socket_close($conn);
+    // Send Commands
+    $socket = unix_socket(SOCK_PATH);
+    if ($socket) {
+        send_command($socket, "add symbol 1 BTC_USDT");
+        send_command($socket, "add book 1");
+        send_command($socket, "add limit sell 1 1 20000 1");
+        echo(read_response($socket, "get book 1"));
+        send_command($socket, "add limit buy 1 1 20000 1");
+        echo(read_response($socket, "get book 1"));
+        socket_close($socket);
     }
     
 ?>
