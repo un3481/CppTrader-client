28c5e1dbbc039a3bdbdcf9ee659ed6bcba9441a6
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-06 17:13:17 -0300

    Update client.py

--- client.py
+++ client.py
@@ -30,18 +30,16 @@ try:
     while True:
         try:
             x = input("> ") # Input command
-            if x != "":
-                buffer = bytes(map(ord, x + "\0")) # Create buffer
-                buffer += b"\0" * (MSG_SIZE - len(buffer)) # enforce MSG_SIZE
-                client.send(buffer) # Send message
-                if x == "exit": break
-                if x.startswith("get"): # If command recieves response
-                    buffer = client.recv(MSG_SIZE_LARGE) # Read MSG_SIZE_LARGE bytes
-                    res = ''.join(map(chr, buffer)).split("\0")[0] # Remove extra chars
-                    print(res) # Print response
+            buffer = bytes(map(ord, x + "\0")) # Create buffer
+            buffer += b"\0" * (MSG_SIZE - len(buffer)) # enforce MSG_SIZE
+            client.send(buffer) # Send message
+            if x == "exit": break
+            if x.startswith("get"): # If command recieves response
+                buffer = client.recv(MSG_SIZE_LARGE) # Read MSG_SIZE_LARGE bytes
+                res = ''.join(map(chr, buffer)).split("\0")[0] # Remove extra chars
+                print(res) # Print response
         
         except KeyboardInterrupt:
-            print("\n")
             break
         except Exception as e:
             print(f"{e}")
