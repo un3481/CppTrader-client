6c3af98a3783c1c5d4e80c87239a815ec4eab1ee
rendezvous <78743473+un3481@users.noreply.github.com>
2022-08-06 16:10:26 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,32 @@
- 
+# -*- coding: utf-8 -*-
+
+import os
+import socket
+
+MSG_SIZE = 1024
+MSG_SIZE_LARGE = 8192
+socket_path = "/home/ubuntu/GitHub/daemons/testing.sock"
+
+if os.path.exists(socket_path):
+    
+    client = socket.socket(
+        socket.AF_UNIX,
+        socket.SOCK_STREAM
+    )
+    client.connect(socket_path)
+
+    while True:
+        try:
+            x = input("> ")
+            if x != "":
+                client.send(x + "\n")
+                if x == "exit": break
+                if x.startswith("get"):
+                    res = client.recv(MSG_SIZE_LARGE)
+                    print(res)
+        except KeyboardInterrupt: break
+
+    client.close()
+    print("Shutdown")
+else:
+    print("Couldn't Connect!")
